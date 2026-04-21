84c65a13fb7bd6094b53b73cbcc9a7e38bec715d
Prashant <prashant78907890@gmail.com>
2026-04-21 21:19:16 +0530

    Stop tracking constants.dart

--- lib/utils/constants.dart
+++
@@ -1,5 +0,0 @@
-class Constants {
-  static const String newsApiKey = '35d4f6c0966f492a9bf6f9c4c182cba5';
-  static const String coingeckoBaseUrl = 'https://api.coingecko.com/api/v3';
-  static const String newsBaseUrl = 'https://newsapi.org/v2';
-}
